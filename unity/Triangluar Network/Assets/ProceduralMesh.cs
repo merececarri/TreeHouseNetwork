41e79f7667ce9b539583b2a54044901087e9b7c5
Mercedes CARRIQUIRY <30964235+merececarri@users.noreply.github.com>
2020-07-15 20:01:43 -0300

    startup triangle
    triangular network base

---
+++ unity/Triangluar Network/Assets/ProceduralMesh.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Start()
+    {
+        MakeMeshData();
+        UpdateMesh();
+    }
+    void MakeMeshData()
+    {
+        //array or vertices
+        vertices = new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 1) };
+        //array of integeres
+        triangles = new int[] { 0, 1, 2 };
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+}
+
